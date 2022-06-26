4865fc26478335f3ffcad0a8f1349136c4c5dccf
Hogobogobo <lawrencemcg@gmail.com>
2022-06-26 18:33:55 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,46 +1,114 @@
-let audioContext;
+// Set up the AudioContext and the dac.
+const audioCtx = new AudioContext();
+const dac = audioCtx.destination;
 
-function play(frequency, time) {
-  const osc =  audioContext.createOscillator();
-  const volume =  audioContext.createGain();
-  osc.type = 'square';
-  osc.frequency.value = frequency;
-  osc.connect(volume);
-  volume.connect(audioContext.destination);
-  //automate the volume
-  volume.gain.setValueAtTime(0,time);
-  volume.gain.linearRampToValueAtTime(1,3 + time);
-  volume.gain.linearRampToValueAtTime(0,6 + time);
 
-  osc.start();
+// Save a space for the audio data.
+let buffer;
+// The window shape to be used by `.setValueCurveAtTime()`.
+let curve = new Float32Array([0, 0.3, 1, 0.3, 0]);
 
-}
+// Load the sound file, and store the data to our `buffer` variable.
+const request = new XMLHttpRequest();
+request.open("get", 'https://bjarnig.s3.eu-central-1.amazonaws.com/sounds/snd.mp3', true);
+request.responseType = "arraybuffer";
+request.onload = () => audioCtx.decodeAudioData(request.response, (data) => buffer = data);
+request.send();
 
-function sequence(){
-  console.log('sequence');
-  // audioContext = new AudioContext();
+// Play a random grain.
+const playGrain = (startTime, grainDuration, pos) => {
 
-  [100,200,80,60,150].forEach((item, index) => {
-    play(item*Math.random(),audioContext.currentTime + (index*2));
-  });
+  // Create a node to play from a buffer.
+  const grain = audioCtx.createBufferSource();
+  grain.buffer = buffer;
 
-}
+  // Create a node to control the buffer's gain.
+  const grainGain = audioCtx.createGain()
+  grainGain.connect(dac);
 
-function saw(frequencies, duration){
-  const osc =  audioContext.createOscillator();
-  osc.type = 'sawtooth';
-  osc.frequency.setValueCurveAtTime(frequencies, audioContext.currentTime,duration);
+  // Create a window.
+  grainGain.gain.setValueAtTime(0, startTime);
+  grainGain.gain.setValueCurveAtTime(curve, startTime, grainDuration);
 
-  osc.connect(audioContext.destination);
-  osc.start();
-}
+  // put it randomly in the stereo field
+  const panNode = audioCtx.createStereoPanner();
+  let panVal = (((Math.random() * 2) - 1) + pos);
+  if (panVal >= 0){
+    panNode.pan.value =  (panVal % 1);
 
-function curve(){
-  audioContext = new AudioContext;
-  let list = [100,200,300].map(function(item) {return item*Math.random()});
-  saw(list, 5 * Math.random());
+  } else{
+    panNode.pan.value =  (1 - (panVal % 1))*(-1);
+  };
 
-}
+  grain.connect(panNode);
+  panNode.connect(grainGain);
+  // Choose a random place to start.
+  const offset = Math.random() * (buffer.duration - grainDuration);
 
-document.getElementById('buttonSequence').addEventListener("click", sequence);
-document.getElementById('buttonCurve').addEventListener("click", curve);
+  // Play the grain.
+  grain.start(startTime, offset, grainDuration);
+};
+
+
+// Run the granulation when you click the play button.
+$("#play").on("click", () => {
+  // Get overlap time, grain duration, and total time from input sliders
+  const overlapTime = Number($("#overlap-time").val() / 1000);
+  const grainDuration = Number($("#grain-duration").val() / 1000);
+  const totalTime = Number($("#total-time").val());
+  const position =  Number($("#pos").val()/100);
+  // const gateChance = Number($("#reverb-gate-chance").val() / 100);
+  const numberOfGrains = (totalTime - grainDuration) / overlapTime;
+
+  // Perform the granulation
+  for (let i = 0; i < numberOfGrains; i += 1) {
+    playGrain(audioCtx.currentTime + i * overlapTime, grainDuration, position);
+  }
+});
+
+
+// Handle slider UI.
+$("#second-level").on("change", () => {
+  $("#second-level-indicator").text($("#second-level").val());
+  curve[1] =  $("#second-level").val()/100;
+});
+$("#beforelast-level").on("change", () => {
+  $("#beforelast-level-indicator").text($("#beforelast-level").val());
+  curve[3] =  $("#second-level").val()/100;
+});
+$("#exponent").on("change", () => {
+  $("#exponent-indicator").text($("#exponent").val());
+  for(let i =0; i < curve.length; i++ ){
+    curve[i] =  Math.pow(curve[i], $("#exponent").val());
+  };
+});
+$("#overlap-time").on("change", () => {
+  $("#overlap-time-indicator").text($("#overlap-time").val() + " ms");
+});
+$("#grain-duration").on("change", () => {
+  $("#grain-duration-indicator").text($("#grain-duration").val() + " ms");
+});
+// $("#reverb-gate-chance"). on("change", () => {
+//   $("reverb-gate-chance-indicator").text($("#reverb-gate-chance").val() +  " %");
+// });
+let cent=100;
+$("#pos").on("change", () => {
+  if ($("#pos").val()>= 50){
+    cent = (100 - $("#pos").val());
+  }else {
+    cent =  $("#pos").val();
+    };
+  $("#pos-indicator").text("Left: " + (100 - $("#pos").val() + "%") +" | center: " + cent + "%" + " | Right: " +  $("#pos").val()+ "%");
+});
+$("#total-time").on("change", () => {
+  $("#total-time-indicator").text($("#total-time").val() + " seconds");
+});
+
+$("#overlap-time-indicator").text($("#overlap-time").val() + " ms");
+$("#grain-duration-indicator").text($("#grain-duration").val() + " ms");
+$("#pos-indicator").text("Left: " + (100 - $("#pos").val() + "%") +" | center: " + cent + "%" + " | Right: " +  $("#pos").val()+ "%");
+// $("#reverb-gate-chance-indicator").text($("#reverb-gate-chance").val() + " %");
+$("#total-time-indicator").text($("#total-time").val() + " seconds");
+
+// Listen to the click event of the button
+// document.getElementById("impulseB").addEventListener("click", loadImpulse);
